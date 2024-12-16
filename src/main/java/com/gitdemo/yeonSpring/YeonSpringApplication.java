dcc1067872578ee948620ec8dfe4871ae0916e02
yeon <rladusgml129@naver.com>
2024-12-16 16:36:43 +0900

    first

---
+++ src/main/java/com/gitdemo/yeonSpring/YeonSpringApplication.java
@@ -0,0 +1,13 @@
+package com.gitdemo.yeonSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeonSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeonSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitdemo/yeonSpring/YeonSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitdemo.yeonSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeonSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
